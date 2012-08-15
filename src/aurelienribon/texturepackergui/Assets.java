ef5cc8abc5d39cbb776b56095f044c788dc79aff
AurelienRibon <AurelienRibon@localhost>
2012-08-15 18:50:30 +0200

    added a splash screen to replace the missing logo

--- src/aurelienribon/texturepackergui/Assets.java
+++ src/aurelienribon/texturepackergui/Assets.java
@@ -2,6 +2,7 @@ package aurelienribon.texturepackergui;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 /**
  * @author Aurelien Ribon | http://www.aurelienribon.com/
@@ -11,18 +12,15 @@ public class Assets extends AssetManager {
 
 	public static void loadAll() {
 		manager = new AssetManager();
-
-		String[] textures = new String[] {
-			"res/data/transparent-light.png",
-			"res/data/transparent-dark.png",
-			"res/data/white.png"
-		};
-
-		for (String tex : textures) manager.load(tex, Texture.class);
+		manager.load("res/data/transparent-light.png", Texture.class);
+		manager.load("res/data/transparent-dark.png", Texture.class);
+		manager.load("res/data/white.png", Texture.class);
+		manager.load("res/data/splash.pack", TextureAtlas.class);
 		while (!manager.update()) {}
 	}
 
 	public static Texture getTransparentLightTex() {return manager.get("res/data/transparent-light.png", Texture.class);}
 	public static Texture getTransparentDarkTex() {return manager.get("res/data/transparent-dark.png", Texture.class);}
 	public static Texture getWhiteTex() {return manager.get("res/data/white.png", Texture.class);}
+	public static TextureAtlas getSplashAtlas() {return manager.get("res/data/splash.pack", TextureAtlas.class);}
 }
